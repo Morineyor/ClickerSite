90adef2ad51b13db9acdd3b846f0f527671d7a52
Morineyor <shashkoff.danilka@gmail.com>
2024-05-28 04:08:10 +0300

    Start

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let counter = 0;
+let clickerButton = document.getElementById('main-b');
+let counterDisplay = document.getElementById('counterDisplay');
+
+
+for (let i = 0; i <= 10; i++){
+    let result = i % 2
+    alert(result)
+}
